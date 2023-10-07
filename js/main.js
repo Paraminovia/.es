e343fc97df2169e68e5ba1a299a8a4d07d5aca8d
Paraminovia <147268508+Paraminovia@users.noreply.github.com>
2023-10-07 16:46:21 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function abrir(){
+    document.getElementById("Open").style.display="block";
+}
+function abrir2(){
+    document.getElementById("Open2").style.display="block";
+}
+function exit(){
+    document.getElementById("Open").style.display="none";
+}
+function exit2(){
+    document.getElementById("Open2").style.display="none";
+}
+
+window.addEventListener('scroll',Function(){
+
+
+
+    
+
+})
